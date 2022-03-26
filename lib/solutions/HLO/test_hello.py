5e7b48710fcba3a716c4481207afcf1c9fd93846
sbx_user1051 <sbx_user1051@169.254.2.189>
2022-03-26 14:45:54 +0000

    Sat Mar 26 14:45:54 UTC 2022

--- lib/solutions/HLO/test_hello.py
+++ lib/solutions/HLO/test_hello.py
@@ -6,11 +6,15 @@ class TestHello(unittest.TestCase):
 
     def test_hello(self):
         res = hello("Alec")
-        self.assertEquals(res, "Hello, World!")
+        self.assertEquals(res, "Hello, Alec!")
+
+    def test_hello_John(self):
+        res = hello("John")
+        self.assertEquals(res, "Hello, John!")
     
     def test_hello_empty(self):
         res = hello("")
-        self.assertEquals(res, "Hello, World!")
+        self.assertEquals(res, "Hello, !")
 
     def test_hello_wrong_type(self):
         with self.assertRaises(AssertionError):
@@ -19,3 +23,4 @@ class TestHello(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+

b6e60d7e59265e847f424bef06f8e2054ae0a7af
sbx_user1051 <sbx_user1051@169.254.2.189>
2022-03-26 14:19:18 +0000

    Sat Mar 26 14:19:18 UTC 2022

--- lib/solutions/SUM/test_sum_solution.py
+++ lib/solutions/SUM/test_sum_solution.py
@@ -1,11 +1,20 @@
 import unittest
 import sum_solution
 
+# Having problems with with python finding the solutions library... Doing testing here to save time
 
 class TestSum(unittest.TestCase):
     def test_sum(self):
         res = sum_solution.compute(1, 2)
         self.assertEquals(res, 3)
+
+    def test_sum_incorrect(self):
+        res = sum_solution.compute(10, 2)
+        self.assertNotEquals(res, 5)
+
+    def test_sum_negative(self):
+        with self.assertRaises(AssertionError):
+            res = sum_solution.compute(-5, 2)
 
 if __name__ == "__main__":
     unittest.main()

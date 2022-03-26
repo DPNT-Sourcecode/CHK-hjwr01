c70a82e2c10abfa5ba83ef8edf96ec424d1a476e
sbx_user1051 <sbx_user1051@169.254.2.189>
2022-03-26 14:22:18 +0000

    Sat Mar 26 14:22:18 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,12 +1,17 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-import os
-import sys
-
-file_dir = os.path.dirname(__file__)
-sys.path.append(file_dir)
 
 
 def compute(x: int, y: int):
-    raise NotImplementedError()
+    """
+    Accepts two positive int arguments and returns the sum. 
+    The magnitude of both integers must be less than 100.
 
+    :param x: first positive integer to sum
+    :param y: second positive integer to sum
+    
+    """
+    assert isinstance(x, int)
+    assert isinstance(y, int)
+    assert x >= 0 and y >= 0, f"Both arguemnts must bo non negative integers, not {x}, {y}"
 
+    return x + y
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -10,6 +10,3 @@ class TestSum(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-
-

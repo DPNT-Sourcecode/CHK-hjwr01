3f5aff6a84911fd1b374c336e5a06a5cd55af664
sbx_user1051 <sbx_user1051@169.254.2.189>
2022-03-26 14:54:15 +0000

    Sat Mar 26 14:54:15 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,13 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+def checkout(skus: str) -> int:
+    """
+    Given a string of sku codes, delimited by space character
+    return the total cost.
+
+    :param skus: str of sku values
+    
+    """
     raise NotImplementedError()
+
--- lib/solutions/CHK/test_checkout.py
+++ lib/solutions/CHK/test_checkout.py
@@ -5,3 +5,14 @@ from checkout_solution import checkout
 class TestCheckout(unittest.TestCase):
     def test_checkout(self):
         res = checkout("A B C D C C C")
+        self.assertEquals(res, 175)
+
+    def test_checkout_empty(self):
+        res = checkout("")
+        self.assertEquals(res, 0)
+
+    def test_checkout_wrong_format(self):
+        with self.assertRaises(AssertionError):
+            res = checkout([])
+        
+
